b55fd0fe58f4c21c8d3be1e4bdd0b049fb0f07ef
Alonge Oluwatobiloba <127941765+Parkoba@users.noreply.github.com>
2023-05-12 14:01:07 +0100

    Commit

--- js/main.js
+++ js/main.js
@@ -39,11 +39,28 @@ window.addEventListener('resize', function () {
 });
 
 const form = document.querySelector('form');
-
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    const email = form.email.value;
-    const password = form.password.value;
-    console.log(`Email: ${email} Password: ${password}`);
-    // Your login validation code goes here...
+    const email = form.querySelector('[name="email"]').value;
+    const password = form.querySelector('[name="password"]').value;
+    const Number = form.querySelector('[name="Number"]').value;
+    const text = form.querySelector('[name="text"]').value;
+    console.log(`Email: ${email} Password: ${password} Number: ${Number} Text: ${text}`);
 });
+
+
+// function onSignIn(googleUser) {
+//     // Get the Google user's ID token
+//     var id_token = googleUser.getAuthResponse().id_token;
+//     // Send the ID token to your server for authentication
+//     // You can use a library like Axios or Fetch to make the API call
+//     axios.post('/api/google-auth', {
+//         id_token: id_token
+//     }).then(function (response) {
+//         // Handle the response from your server
+//         console.log(response.data);
+//     }).catch(function (error) {
+//         // Handle errors
+//         console.log(error);
+//     });
+// }

227d944ba27979f9042a0e23ff4e2a4c2f98b192
Alonge Oluwatobiloba <127941765+Parkoba@users.noreply.github.com>
2023-06-02 15:38:33 +0100

    /commit

---
+++ js/map.js
@@ -0,0 +1,16 @@
+function initMap(){
+// map = new google.maps.Map(document.getElementById('map')), {    
+// }
+
+var options = {
+    center: {
+        lat: 38.3460, 
+        lng: -0.4907
+    },
+    zoom: 8
+}
+
+
+map = new google.maps.Map(document.getElementById("map") , options)
+
+}

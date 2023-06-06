50510237e26c30c06da4f1c7f6c215ce67f54fe4
Alonge Oluwatobiloba <127941765+Parkoba@users.noreply.github.com>
2023-06-06 09:06:03 +0100

    Commit

--- js/main.js
+++ js/main.js
@@ -41,16 +41,16 @@ window.addEventListener('resize', function () {
 //console Signup Showcase 
 const form = document.querySelector('form');
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault();
 
-    const email = form.querySelector('[name="email"]').value;
-    const password = form.querySelector('[name="password"]').value;
-    const number = form.querySelector('[name="number"]').value;
-    const text = form.querySelector('[name="text"]').value;
+//     const email = form.querySelector('[name="email"]').value;
+//     const password = form.querySelector('[name="password"]').value;
+//     const number = form.querySelector('[name="number"]').value;
+//     const text = form.querySelector('[name="text"]').value;
 
-    console.log(`Email: ${email} Password: ${password} Number: ${number} Text: ${text}`);
-});
+//     console.log(`Email: ${email} Password: ${password} Number: ${number} Text: ${text}`);
+// });
 
 
 // function onSignIn(googleUser) {

df6959bd64e3165ff6d289a9bd71367a7df248d2
Alonge Oluwatobiloba <127941765+Parkoba@users.noreply.github.com>
2023-12-21 12:10:03 +0100

    comnmit

--- js/main.js
+++ js/main.js
@@ -37,34 +37,3 @@ window.addEventListener('resize', function () {
         showMenu = false;
     }
 });
-
-//console Signup Showcase 
-const form = document.querySelector('form');
-
-// form.addEventListener('submit', (event) => {
-//     event.preventDefault();
-
-//     const email = form.querySelector('[name="email"]').value;
-//     const password = form.querySelector('[name="password"]').value;
-//     const number = form.querySelector('[name="number"]').value;
-//     const text = form.querySelector('[name="text"]').value;
-
-//     console.log(`Email: ${email} Password: ${password} Number: ${number} Text: ${text}`);
-// });
-
-
-// function onSignIn(googleUser) {
-//     // Get the Google user's ID token
-//     var id_token = googleUser.getAuthResponse().id_token;
-//     // Send the ID token to your server for authentication
-//     // You can use a library like Axios or Fetch to make the API call
-//     axios.post('/api/google-auth', {
-//         id_token: id_token
-//     }).then(function (response) {
-//         // Handle the response from your server
-//         console.log(response.data);
-//     }).catch(function (error) {
-//         // Handle errors
-//         console.log(error);
-//     });
-// }
---
+++ js/sign-in.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+    $("#do_login").click(function () {
+        closeLoginInfo();
+        var parent = $(this).parent();
+        var span = parent.find('span');
+
+        span.css("display", "none").removeClass("i-save i-warning i-close");
+
+        var proceed = true;
+        $("#login_form input").each(function () {
+            var input = $(this);
+            if (!$.trim(input.val())) {
+                input.parent().find('span').addClass("i-warning").css("display", "block");
+                proceed = false;
+            }
+        });
+
+        if (proceed) {
+            span.addClass("i-save").css("display", "block");
+        }
+    });
+
+    // Reset previously results and hide all messages on .keyup()
+    $("#login_form input").keyup(function () {
+        $(this).parent().find('span').css("display", "none");
+    });
+
+    openLoginInfo();
+    setTimeout(closeLoginInfo, 1000);
+});
+
+function openLoginInfo() {
+    $('.b-form').css("opacity", "0.01");
+    $('.box-form').css("left", "-37%");
+    $('.box-info').css("right", "-37%");
+}
+
+function closeLoginInfo() {
+    $('.b-form').css("opacity", "1");
+    $('.box-form').css("left", "0px");
+    $('.box-info').css("right", "-5px");
+}
+
+$(window).on('resize', function () {
+    closeLoginInfo();
+});
+
+// Placeholder functions for the onclick events
+function forgotPassword() {
+    // Replace the alert with your logic for "Forgot Password?"
+    alert("");
+}
+
+function contactSupport() {
+    // Replace the alert with your logic for "Contact Support"
+    alert("Implement Contact Support functionality here.");
+}
+
+function createAccount() {
+    // Replace the alert with your logic for "CREATE ACCOUNT"
+    alert("Implement CREATE ACCOUNT functionality here.");
+}
